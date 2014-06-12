e1ca2ad1f0f7f5679ba7294be462a97f7999c433
Graham <graham_howden1@yahoo.co.uk>
2014-06-12 23:15:41 +0100

    hacky script to convert int -> varint

--- src/Server/DataTypes/VarInt.php
+++ src/Server/DataTypes/VarInt.php
@@ -23,15 +23,76 @@ class VarInt extends DataType {
             if(!$data) {
                 throw new NoDataException();
             }
+            //ascii char
             $data = ord($data);
+
+            //if only I knew...
             $result |= ($data & 0x7F) << $i++ * 7;
+
+            //if it's too large
             if( $i > 5 ) {
                 throw new InvalidDataException();
             }
+
+            //also probably pretty important
             if(($data & 0x80) != 128) {
                 break;
             }
         }
         return new VarInt($result, $i);
+    }
+
+    public static function fromInt($int)
+    {
+        $string = decbin($int);
+        if (strlen($string) < 8)
+        {
+            $hexstring = dechex(bindec($string));
+            if (strlen($hexstring) % 2 == 1)
+                $hexstring = '0' . $hexstring;
+            return self::hex_to_str($hexstring);
+        }
+
+        // split it and insert the mb byte
+        $string_array = array();
+        $pre = '1';
+        while (strlen($string) > 0)
+        {
+            if (strlen($string) < 8)
+            {
+                $string = substr('00000000', 0, 7 - strlen($string) % 7) . $string;
+                $pre = '0';
+            }
+            $string_array[] = $pre . substr($string, strlen($string) - 7, 7);
+            $string = substr($string, 0, strlen($string) - 7);
+            $pre = '1';
+            if ($string == '0000000')
+                break;
+        }
+
+        $hexstring = '';
+        foreach ($string_array as $string)
+        {
+            $hexstring .= sprintf('%02X', bindec($string));
+        }
+
+
+        return self::hex_to_str($hexstring);
+    }
+
+    /**
+     * Converts hex 2 ascii
+     * @param String $hex - the hex string
+     * @return string
+     */
+    private static function hex_to_str($hex)
+    {
+        $str = '';
+
+        for($i = 0; $i < strlen($hex); $i += 2)
+        {
+            $str .= chr(hexdec(substr($hex, $i, 2)));
+        }
+        return $str;
     }
 } 
