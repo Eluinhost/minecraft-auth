cbc8e0f9cc21096179bae9ecab548efaa5f5f940
Graham <graham_howden1@yahoo.co.uk>
2014-06-14 10:55:58 +0100

    expand test cases

--- tests/ReactServer/DataTypes/VarIntTest.php
+++ tests/ReactServer/DataTypes/VarIntTest.php
@@ -10,12 +10,20 @@ class VarIntTest extends \PHPUnit_Framework_TestCase {
         //hex = 0x0F
         //bin = 0000 1111
         $data = hex2bin('0F');
-
-        echo "DATA: ".ord($data);
-
         $decoded = VarInt::readUnsignedVarInt($data);
-
         $this->assertEquals(15, $decoded->getValue());
+        $this->assertEquals(hex2bin('0F'), $decoded->getEncoded());
+        $this->assertEquals(1, $decoded->getDataLength());
+
+        //decoded = 0
+        //dec = 0
+        //hex = 0x00
+        //bin = 0000 0000
+        $data = hex2bin('00');
+        $decoded = VarInt::readUnsignedVarInt($data);
+        $this->assertEquals(0, $decoded->getValue());
+        $this->assertEquals(hex2bin('00'), $decoded->getEncoded());
+        $this->assertEquals(1, $decoded->getDataLength());
     }
 
     public function testVarIntParseMulti()
@@ -27,20 +35,45 @@ class VarIntTest extends \PHPUnit_Framework_TestCase {
         $data = hex2bin('AC02');
 
         $decoded = VarInt::readUnsignedVarInt($data);
-
         $this->assertEquals(300, $decoded->getValue());
+        $this->assertEquals($data, $decoded->getEncoded());
+        $this->assertEquals(2, $decoded->getDataLength());
+
+        //decoded = 9238
+        //dec = 38472
+        //hex = 0x9648
+        //bin = 1001 0110 0100 1000
+        $data = hex2bin('9648');
+        $decoded = VarInt::readUnsignedVarInt($data);
+        $this->assertEquals(9238, $decoded->getValue());
+        $this->assertEquals($data, $decoded->getEncoded());
+        $this->assertEquals(2, $decoded->getDataLength());
     }
 
     public function testVarIntGenerateSingle()
     {
-        $encoded = VarInt::writeUnsignedVarInt(1);
-        $this->assertEquals(1, $encoded->getValue());
+        $encoded = VarInt::writeUnsignedVarInt(0);
+        $this->assertEquals(0, $encoded->getValue());
+        $this->assertEquals(hex2bin('00'), $encoded->getEncoded());
+        $this->assertEquals(1, $encoded->getDataLength());
+
+        $encoded = VarInt::writeUnsignedVarInt(15);
+        $this->assertEquals(15, $encoded->getValue());
+        $this->assertEquals(hex2bin('0F'), $encoded->getEncoded());
+        $this->assertEquals(1, $encoded->getDataLength());
     }
 
     public function testVarIntGenerateMulti()
     {
         $encoded = VarInt::writeUnsignedVarInt(300);
+        $this->assertEquals(300, $encoded->getValue());
         $this->assertEquals(hex2bin('AC02'), $encoded->getEncoded());
+        $this->assertEquals(2, $encoded->getDataLength());
+
+        $encoded = VarInt::writeUnsignedVarInt(9238);
+        $this->assertEquals(9238, $encoded->getValue());
+        $this->assertEquals(hex2bin('9648'), $encoded->getEncoded());
+        $this->assertEquals(2, $encoded->getDataLength());
     }
 
     public function testVarIntParsePartialSingle()
@@ -50,7 +83,6 @@ class VarIntTest extends \PHPUnit_Framework_TestCase {
         $data = hex2bin('80');
 
         $decoded = VarInt::readUnsignedVarInt($data);
-
         $this->assertFalse($decoded);
     }
 
@@ -61,12 +93,12 @@ class VarIntTest extends \PHPUnit_Framework_TestCase {
         $data = hex2bin('969480');
 
         $decoded = VarInt::readUnsignedVarInt($data);
-
         $this->assertFalse($decoded);
     }
 
     public function testVarIntTooLarge()
     {
+        //more than 5 bytes = failure
         //bin - 1000 0000 1000 0000 1000 0000 1000 0000 1000 0000 1000 0000
         //hex - 0x808080808080
         $data = hex2bin('808080808080');
